3c119bf2dc5fc5d07005928957a61f1bc351a420
Macallister Armstrong <macallister.armstrong@gmail.com>
2020-09-19 16:11:42 -0400

    adding prev helloWorld assignment as template

---
+++ cmake-build-debug/CMakeFiles/CS4541_MacallisterArmstrong_Assignment_1_Number_Demonstrations.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS4541_MacallisterArmstrong_Assignment_1_Number_Demonstrations.dir/main.c.o"
+  "CS4541_MacallisterArmstrong_Assignment_1_Number_Demonstrations"
+  "CS4541_MacallisterArmstrong_Assignment_1_Number_Demonstrations.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS4541_MacallisterArmstrong_Assignment_1_Number_Demonstrations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
